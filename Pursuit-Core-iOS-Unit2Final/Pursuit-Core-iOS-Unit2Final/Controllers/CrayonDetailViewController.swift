8132836a82b9c1e55fb0ad8bb212fc8fa8ff76e8
yazzy4 <yazzy4@gmail.com>
2018-11-30 17:35:48 -0500

    Update

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonDetailViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonDetailViewController.swift
@@ -12,6 +12,7 @@ class CrayonDetailViewController: UIViewController {
 
     public var crayon: Crayon?
     
+    
     @IBOutlet weak var colorLabel: UILabel!
     @IBOutlet weak var sliderControl: UISlider!
     @IBOutlet weak var sliderLabel: UILabel!
@@ -24,6 +25,10 @@ class CrayonDetailViewController: UIViewController {
     
     @IBOutlet var background: UIView!
     
+    var currentSliderValue: Double = 1.0
+    var red:CGFloat = 0.0
+    var blue:CGFloat = 0.0
+    var green:CGFloat = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +38,7 @@ class CrayonDetailViewController: UIViewController {
         sliderControl.value = 0
         sliderControl.minimumValue = 0
         sliderControl.maximumValue = 10
+        var currentSliderValue: Double = 1.0
         
         sliderLabel2.text = "0"
         sliderControl2.value = 0
@@ -48,43 +54,118 @@ class CrayonDetailViewController: UIViewController {
         sliderControl3.value = 0
         sliderControl3.minimumValue = 0
         sliderControl3.maximumValue = 10
+        
+        
+       
+    
 
     }
     
+   
     public func updateCrayonUI(){
         colorLabel.text = crayon?.name
         let colors = UIColor.init(displayP3Red: CGFloat(crayon!.red/255), green:CGFloat(crayon!.green/255), blue:CGFloat(crayon!.blue/255), alpha: 1.0)
         background.backgroundColor = colors
         
-         
         
     }
 
-//    private func updateRecipeUI() {
-//        crayonImage.image = recipe?.image
-//        recipeName.text = recipe?.name
-//        recipeDescription.text = recipe?.description
-//    }
-    
     
     @IBAction func sliderChanged(_ sender: UISlider) {
-       sliderLabel.text = Int(sender.value).description
+       sliderLabel.text = Double(sender.value).description
+//       var colorValue = CGFloat(sliderControl.value)
+        if sender.tag == 1{
+            crayon?.red = Double(CGFloat(sender.value/255))        }else if sender.tag == 2{
+            crayon?.green = Double(CGFloat(sender.value/255))
+        }else{
+            crayon?.blue = Double(CGFloat(sender.value/255))
+        }
+        
+        let backgroundColor = UIColor(red: self.red, green: self.green, blue: self.blue, alpha: 1.0)
+        
+        self.view.backgroundColor = backgroundColor
     }
+        
+//        self.view.backgroundColor = UIColor.init(displayP3Red: CGFloat(sliderControl.value/255), green:CGFloat (sliderControl2.value/255), blue: CGFloat(sliderControl3.value/255), alpha: 1)    }
     
-    @IBAction func slider2Changed(_ sender: UISlider) {
-        sliderLabel2.text = Int(sender.value).description
-    }
-    
+
+@IBAction func slider2Changed(_ sender: UISlider) {
+     sliderLabel2.text = Double(sender.value).description
+        if sender.tag == 1{
+            crayon?.red = Double(CGFloat(sender.value/255))
+        }else if sender.tag == 2{
+            crayon?.green = Double(CGFloat(sender.value/255))
+        }else{
+            crayon?.blue = Double(CGFloat(sender.value/255))
+        }
+        
+        let backgroundColor = UIColor(red: self.red, green: self.green, blue: self.blue, alpha: 1.0)
+        
+        self.view.backgroundColor = backgroundColor
+
+}
    
+    
     @IBAction func stepperChanged(_ sender: UIStepper) {
-        stepperLabel.text = Int(sender.value).description
+       stepperLabel.text = Double(sender.value).description
+        if sender.tag == 1{
+            crayon?.red = Double(CGFloat(sender.value/255))
+        }else if sender.tag == 2{
+            crayon?.green = Double(CGFloat(sender.value/255))
+        }else{
+            crayon?.blue = Double(CGFloat(sender.value/255))
+        }
+        
+        let backgroundColor = UIColor(red: self.red, green: self.green, blue: self.blue, alpha: 1.0)
+        
+        self.view.backgroundColor = backgroundColor
         
     }
-   
+
+
+    
     @IBAction func slider3Changed(_ sender: UISlider) {
-        sliderLabel3.text = Int(sender.value).description
+        sliderLabel3.text = Double(sender.value).description
+        if sender.tag == 1{
+            crayon?.red = Double(CGFloat(sender.value/255))
+        }else if sender.tag == 2{
+            crayon?.green = Double(CGFloat(sender.value/255))
+        }else{
+            crayon?.blue = Double(CGFloat(sender.value/255))
+        }
+        
+        let backgroundColor = UIColor(red: self.red, green: self.green, blue: self.blue, alpha: 1.0)
+        
+        self.view.backgroundColor = backgroundColor
+        
     }
-    
-    
-    
+
 }
+    
+    
+
+
+
+//IBAction func redSliderChanged(_ sender: UISlider) {
+//    redLabel.text = Double(sender.value).description
+//    self.view.backgroundColor = UIColor.init(displayP3Red: CGFloat(redSlider.value / 255), green: CGFloat(greenSlider.value / 255), blue: CGFloat(blueSlider.value / 255), alpha: 1)
+//}
+//@IBAction func greenSliderChanged(_ sender: UISlider) {
+//    greenLabel.text = Double(sender.value).description
+//    self.view.backgroundColor = UIColor.init(displayP3Red: CGFloat(redSlider.value / 255), green: CGFloat(greenSlider.value / 255), blue: CGFloat(blueSlider.value / 255), alpha: 1)
+//}
+//@IBAction func blueSliderChanged(_ sender: UISlider) {
+//    blueLabel.text = Double(sender.value).description
+//    self.view.backgroundColor = UIColor.init(displayP3Red: CGFloat(redSlider.value / 255), green: CGFloat(greenSlider.value / 255), blue: CGFloat(blueSlider.value / 255), alpha: 1)
+//}
+//@IBAction func alphaStepperChanged(_ sender: UIStepper) {
+//    alphaLabel.text = Double(sender.value).description
+//}
+//
+//
+//
+//}
+//
+//
+//}
+
